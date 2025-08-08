ab6fba48b5a7220ad07203ce7e8ccbfa246050b9
Brian Tan <bharveytan@gmail.com>
2025-08-08 16:04:03 -0700

    testing out Dash

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from dash import Dash, html
+
+app = Dash()
+
+app.layout = html.Div(children="Hello from m2m-capstone1-dash!")
 def main():
     print("Hello from m2m-capstone1-dash!")
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
