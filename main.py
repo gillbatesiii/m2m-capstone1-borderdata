cc345ff5af0ac33a4b0a63ddf51b01caef6ecdf1
Brian Tan <bharveytan@gmail.com>
2025-08-08 18:16:09 -0700

    load bts keg4-3bc2 (border) data with Socrata client

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from dash import Dash, html
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from sodapy import Socrata
+
+# get APP_TOKEN and initialize Socrata client
+
+load_dotenv()
+
+APP_TOKEN = os.getenv("SOCRATA_APP_TOKEN")
+
+if APP_TOKEN:
+    print("Successfully loaded app token.")
+else:
+    print("Warning: APP_TOKEN not found. Make sure it's set in your .env file.")
+
+client = Socrata("data.bts.gov", APP_TOKEN)
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("keg4-3bc2", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
 
 app = Dash()
-
 app.layout = html.Div(children="Hello from m2m-capstone1-dash!")
+
 def main():
-    print("Hello from m2m-capstone1-dash!")
+    print("m2m-capstone1-dash is running")
     app.run(debug=True)
 
 
