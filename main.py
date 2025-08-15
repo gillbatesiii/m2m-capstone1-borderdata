88ea0985f3339f9bc187756aa0e8a3dc3fb7ef9c
Brian Tan <bharveytan@gmail.com>
2025-08-14 22:09:50 -0700

    added default css styles, add where clause to limit to Canadian border on fetch

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sodapy import Socrata
 # get APP_TOKEN and initialize Socrata client
 
 load_dotenv()
-
+print('appname', os.getenv('app'))
 APP_TOKEN = os.getenv("SOCRATA_APP_TOKEN")
 
 if APP_TOKEN:
@@ -18,17 +18,28 @@ else:
 client = Socrata("data.bts.gov", APP_TOKEN)
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("keg4-3bc2", limit=2000)
-
+results = client.get("keg4-3bc2", limit=4000,
+                     where="border = 'US-Canada Border'")
+print('length', len(results))
 # Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
+
+# Data cleaning
 results_df.drop(columns=["point"], inplace=True)
+results_df['date'] = pd.to_datetime(results_df['date'])
+results_df['date'] = results_df['date'].dt.date
+print('info', results_df.info())
+nulls_df = results_df[results_df.isnull().any(axis=1)]
 app = Dash()
 app.layout = [
-    html.Div(children="Hello from m2m-capstone1-dash!"),
+    html.H1(children="m2m-capstone1-dash!"),
+    html.Div(children="Null values: "),
+    dash_table.DataTable(data=nulls_df.to_dict("records"), page_size=10),
+    html.Hr(),
     dash_table.DataTable(data=results_df.to_dict("records"), page_size=10),
     html.Hr(),
-    dash_table.DataTable(data=results_df.to_dict("records"), columns=[{"name": i, "id": i} for i in results_df.columns]),
+    dash_table.DataTable(data=results_df.to_dict("records"),
+                         columns=[{"name": i, "id": i} for i in results_df.columns]),
 ]
 
 def main():
